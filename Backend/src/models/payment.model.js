2b3824646e87b533ebd67efd2691bb53dc78a30e
sachin katkar <sachinkatkar1976@gmail.com>
2026-09-17 11:31:07 +0530

    normal payment added

--- Backend/src/models/payment.model.js
+++ Backend/src/models/payment.model.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    amount: { type: Number, required: true },
-    orderId: { type: String, required: true },
-    paymentId: { type: String, required: true },
-    year: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+const manualPaymentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    imageurl: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Payment", paymentSchema);
+module.exports = mongoose.model("ManualPayment", manualPaymentSchema);
